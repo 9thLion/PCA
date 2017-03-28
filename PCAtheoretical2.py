f5daae3b932a789bfda351cc65b2e1505b4466e4
9thLion <mondestrasz@gmail.com>
2017-03-28 19:52:55 +0300

    Add files via upload

---
+++ PCAtheoretical2.py
@@ -0,0 +1,35 @@
+import numpy as np
+import PCApackage as pac
+import matplotlib.pyplot as plt
+
+mean = np.ones(10)
+var = np.identity(10)
+X = np.random.multivariate_normal(mean,var,10).T
+Y,S=pac.MyFirstAlgorithm(X,k=1) 
+print(X.shape)
+X2=X[:,:5] #5 samples
+Y2,S2=pac.MyFirstAlgorithm(X2,k=1)
+S=list(S)
+S.sort(reverse=True)
+S2=list(S2)
+S2.sort(reverse=True)
+plt.figure(figsize=(6,8))
+plt.subplot(2,3,1)
+plt.title('Data Distribution')
+plt.hist(X)
+plt.subplot(2,3,2)
+plt.title('Eigenvalues, 10 samples')
+plt.scatter(range(len(S)),S)
+plt.subplot(2,3,3)
+plt.title('After PCA, 10 samples')
+plt.scatter(Y[0],np.zeros(Y.shape[1]), c='c')
+plt.subplot(2,3,4)
+plt.title('Data Distribution')
+plt.hist(X2)
+plt.subplot(2,3,5)
+plt.title('Eigenvalues, 5 samples')
+plt.scatter(range(len(S2)),S2)
+plt.subplot(2,3,6)
+plt.title('After PCA, 5 samples')
+plt.scatter(Y2[0],np.zeros(Y2.shape[1]), c='c')
+plt.show()
