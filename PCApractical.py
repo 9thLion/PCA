b694ff255cdae326f5d00ebebb9e63199b2dde39
9thLion <mondestrasz@gmail.com>
2017-03-28 22:43:30 +0300

    Add files via upload

--- PCApractical.py
+++ PCApractical.py
@@ -2,6 +2,7 @@
 import numpy as np
 import PCApackage as pac 
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 from sklearn.decomposition import PCA
 import os
 
@@ -10,7 +11,6 @@ os.system('gunzip GDS6248.soft.gz')
 os.system('tail -n +141 GDS6248.soft > GDS6248.softer') #getting rid of the redundant lines
 os.system('rm GDS6248.soft')
 os.system('head -n -1 GDS6248.softer > GDS6248.soft') 
-os.system('rm GDS6248.softer')
 
 temp = []
 with open('GDS6248.soft') as f:
@@ -24,17 +24,20 @@ with open('GDS6248.soft') as f:
 		temp.append(temp2)
 
 X=np.array(temp)
-Color = ['w' for x in range(3)] + ['c' for x in range(24)] + ['r' for x in range(24)]
+Color = ['m' for x in range(3)] + ['c' for x in range(24)] + ['r' for x in range(24)]
 
 plt.figure()
 plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.5, hspace=0.5)
 
+green = mpatches.Patch(color='m',label='baseline')
+cyan = mpatches.Patch(color='c',label='normal diet')
+red = mpatches.Patch(color='r',label='high fat diet')
 
 Y=pac.PCA(X,k=2, F=False)[0]
 plt.subplot(2,2,1)
 plt.title('After Linear PCA')
 plt.scatter(Y[0], Y[1], c=Color)
-
+plt.legend(bbox_to_anchor=(1, -.1), loc=2, borderaxespad=0.,handles=[green, cyan, red], prop={'size':10})
 Y=pac.PPCA(X, 2)
 plt.subplot(2,2,2)
 plt.title('After PPCA')
